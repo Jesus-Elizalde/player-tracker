e0176d340a6f7b748c13fba14fd5ab7f887bb1f4
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-03-07 22:40:48 -0800

    adding player router

---
+++ src/server/api/routers/player.ts
@@ -0,0 +1,56 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const playerRouter = createTRPCRouter({
+  getPlayerByTeam: protectedProcedure
+    .input(
+      z.object({
+        teamId: z.string(),
+      })
+    )
+    .query(({ ctx, input }) => {
+      return ctx.prisma.player.findMany({
+        where: {
+          teamId: input.teamId,
+        },
+      });
+    }),
+
+  create: protectedProcedure
+    .input(
+      z.object({
+        firstName: z.string(),
+        lastName: z.string(),
+        teamId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.player.create({
+        data: {
+          firstName: input.firstName,
+          lastName: input.lastName,
+          teamId: input.teamId,
+        },
+      });
+    }),
+
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      const { id } = input;
+      return ctx.prisma.player.findUnique({
+        where: { id },
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.player.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
